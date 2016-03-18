7298d423087899d348e4de0999aabe8fb1134843
Moses <lx0319@gmail.com>
2016-03-18 15:19:16 +0800

    jquery  datatables  返回对象实现。

--- src/main/java/net/liuxuan/springboottest/web/SampleController.java
+++ src/main/java/net/liuxuan/springboottest/web/SampleController.java
@@ -142,30 +142,30 @@ public class SampleController {
 
         List rtnlist = new ArrayList();
 //        Map<String,Object> data = new LinkedHashMap<String, Object>();
-//        for (Person person : personList) {
-//            Map<String,Object> data1 = new LinkedHashMap<String, Object>();
-//            data1.put("Id",person.getId());
-//            data1.put("LastName",person.getFirstName());
-//            data1.put("LastName",person.getFirstName());
-//            data1.put("City",person.getAddress().getTown().getName());
-//            data1.put("Mail",person.getMail());
-//            data1.put("Salary",person.getSalary());
-//            data1.put("BirthDate",person.getBirthDate());
-//            data1.put("Company",person.getCompany().getName());
-//            rtnlist.add(data1);
-//        }
         for (Person person : personList) {
-            List<String> list = new ArrayList<String>();
-            list.add(String.valueOf(person.getId()));
-            list.add(person.getFirstName());
-            list.add(person.getFirstName());
-            list.add(person.getAddress().getTown().getName());
-            list.add(person.getMail());
-            list.add(person.getSalary());
-            list.add(String.valueOf(person.getBirthDate()));
-            list.add(person.getCompany().getName());
-            rtnlist.add(list);
+            Map<String,Object> data1 = new LinkedHashMap<String, Object>();
+            data1.put("Id",person.getId());
+            data1.put("LastName",person.getLastName());
+            data1.put("FirstName",person.getFirstName());
+            data1.put("City",person.getAddress().getTown().getName());
+            data1.put("Mail",person.getMail());
+            data1.put("Salary",person.getSalary());
+            data1.put("BirthDate",person.getBirthDate());
+            data1.put("Company",person.getCompany().getName());
+            rtnlist.add(data1);
         }
+//        for (Person person : personList) {
+//            List<String> list = new ArrayList<String>();
+//            list.add(String.valueOf(person.getId()));
+//            list.add(person.getLastName());
+//            list.add(person.getFirstName());
+//            list.add(person.getAddress().getTown().getName());
+//            list.add(person.getMail());
+//            list.add(person.getSalary());
+//            list.add(String.valueOf(person.getBirthDate()));
+//            list.add(person.getCompany().getName());
+//            rtnlist.add(list);
+//        }
 
 		Map<Object, Object> modelMap = new HashMap<Object, Object>();
 //        ModelMap modelMap = modelAndView.getModelMap();
@@ -180,6 +180,65 @@ public class SampleController {
 
 
 		response.getWriter().write(json);
+        modelAndView.setViewName("test/test");
+
+//		log.info("draw:{},start:{},length:{},search:{}",draw,start,length,search);
+
+//		return "test/test";
+//        return modelAndView;
+    }
+
+    @RequestMapping(value = "dp4A", produces = "application/json")
+    public void ajaxIndexA(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) throws IOException {
+        //获取请求次数
+        String draw = "0";
+        draw = request.getParameter("draw");
+        //数据起始位置
+        String start = request.getParameter("start");
+        //数据长度
+        String length = request.getParameter("length");
+
+        //总记录数
+        String recordsTotal = "500";
+
+        //过滤后记录数
+        String recordsFiltered = "";
+        //定义列名
+        String[] cols = {"id", "LastName", "FirstName", "City", "Mail", "salary", "BirthDate", "Company"};
+        //获取客户端需要那一列排序
+        String orderColumn = "0";
+//		orderColumn = request.getParameter("order[0][column]");
+//		orderColumn = cols[Integer.parseInt(orderColumn)];
+        //获取排序方式 默认为asc
+        String orderDir = "asc";
+        orderDir = request.getParameter("order[0][dir]");
+
+        //获取用户过滤框里的字符
+        String searchValue = request.getParameter("search[value]");
+
+        List<Company> companies = companyRepository.findByName(searchValue);
+
+        List<Person> personList = new ArrayList<Person>();
+
+        for (Company company : companies) {
+            personList.addAll(personRepository.findByCompany(company));
+        }
+        recordsFiltered = String.valueOf(personList.size());
+
+
+        Map<Object, Object> modelMap = new HashMap<Object, Object>();
+//        ModelMap modelMap = modelAndView.getModelMap();
+        modelMap.put("data", personList);
+        modelMap.put("recordsTotal", recordsTotal);
+        modelMap.put("recordsFiltered", recordsFiltered);
+        modelMap.put("draw", draw);
+
+
+        Gson gson = EntityGsonHelper.goGsonWithNoCheck();
+        String json = gson.toJson(modelMap);
+
+
+        response.getWriter().write(json);
         modelAndView.setViewName("test/test");
 
 //		log.info("draw:{},start:{},length:{},search:{}",draw,start,length,search);
