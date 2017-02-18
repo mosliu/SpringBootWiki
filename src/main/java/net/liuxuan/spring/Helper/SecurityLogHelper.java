314a3a98e5d535f53aed49f6ddb04ef36fb2ec96
mosliu <lx0319@gmail.com>
2017-02-18 16:23:23 +0800

    Springboot 提升至1.5.1 更新日志记录功能 修改TranscationManager

---
+++ src/main/java/net/liuxuan/spring/Helper/SecurityLogHelper.java
@@ -0,0 +1,104 @@
+package net.liuxuan.spring.Helper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.google.gson.Gson;
+import net.liuxuan.SprKi.entity.security.LogLevel;
+import net.liuxuan.SprKi.entity.security.SecurityLog;
+import net.liuxuan.SprKi.service.security.SecurityLogService;
+import net.liuxuan.spring.Helper.gson.EntityGsonHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+/**
+ * Copyright (c) 2010-2017.  by Liuxuan   All rights reserved. <br/>
+ * ***************************************************************************
+ * 源文件名:  net.liuxuan.spring.Helper.SecurityLogHelper
+ * 功能:
+ * 版本:	@version 1.0
+ * 编制日期: 2017/2/16 13:11
+ * 修改历史: (主要历史变动原因及说明)
+ * YYYY-MM-DD |    Author      |	 Change Description
+ * 2017/2/16  |    Moses       |     Created
+ */
+@Component
+public class SecurityLogHelper {
+    private static SecurityLogService securityLogService;
+
+    @Autowired
+    public SecurityLogHelper(SecurityLogService securityLogService){
+        SecurityLogHelper.securityLogService = securityLogService;
+    }
+
+    /**
+     * 记录登陆日志
+     *
+     * @param request 调用者的request
+     * @param action action类型，建议使用LogActionType中定义的
+     * @param entity 相关实体类
+     * @param operation 操作记录
+     * @param url 访问地址
+     */
+    public static void LogLogStatus(HttpServletRequest request,String action, Object entity, String operation,String url) {
+        log(request, action, entity, operation,LogLevel.LOGACTIVITYS,url);
+    }
+
+    /**
+     * 记录活动日志
+     *
+     * @param request 调用者的request
+     * @param action action类型，建议使用LogActionType中定义的
+     * @param entity 相关实体类
+     * @param operation 操作记录
+     * @param url 访问地址
+     */
+    public static void LogActivity(HttpServletRequest request,String action, Object entity, String operation,String url) {
+        log(request, action, entity, operation,LogLevel.ACTIVITYS,url);
+    }
+    /**
+     * 记录高权限日志
+     *
+     * @param request 调用者的request
+     * @param action action类型，建议使用LogActionType中定义的
+     * @param entity 相关实体类
+     * @param operation 操作记录
+     * @param url 访问地址
+     */
+    public static void LogHIGHRIGHT(HttpServletRequest request,String action, Object entity, String operation,String url) {
+        log(request, action, entity, operation,LogLevel.HIGHRIGHT,url);
+    }
+
+    public static void log(HttpServletRequest request,String action, Object entity, String operation,String logLevel,String url) {
+//        Gson gson = EntityGsonHelper.goGsonwithEntityCheck(entity.getClass());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Hibernate5Module());
+
+
+        SecurityLog slog = new SecurityLog();
+//        slog.setAction(request.getMethod());
+        slog.setUsername(request.getRemoteUser());
+        slog.setLogIp(RequestHelper.getIpAddr(request));
+        slog.setLogUrl(request.getRequestURI());
+        slog.setLogTime(new Date());
+//        slog.setAction(LogActionType.CREATE_OR_UPDATE);
+        slog.setAction(action);
+
+//        slog.setLogInfo(gson.toJson(entity).toString());
+        try {
+            slog.setLogInfo(mapper.writeValueAsString(entity));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        slog.setLogLevel(logLevel);
+        slog.setOperation(operation);
+        slog.setUrl(url);
+
+        securityLogService.saveSecurityLog(slog);
+    }
+}
