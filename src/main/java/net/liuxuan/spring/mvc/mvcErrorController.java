5d36960f78e47374877a648056d323e52e8c26fe
mosliu <lx0319@gmail.com>
2017-03-22 15:39:41 +0800

    添加ignore

---
+++ src/main/java/net/liuxuan/spring/mvc/mvcErrorController.java
@@ -0,0 +1,16 @@
+package net.liuxuan.spring.mvc;
+
+/**
+ * Copyright (c) 2010-2016.  by Liuxuan   All rights reserved.
+ * ***************************************************************************
+ * 源文件名:  net.liuxuan.spring.mvc.mvcErrorController
+ * 功能:
+ * 版本:	@version 1.0
+ * 编制日期: 2016/2/15 11:04
+ * 修改历史: (主要历史变动原因及说明)
+ * YYYY-MM月DD |    Author      |	 Change Description
+ * 2016/2/15 |    Moses       |     Created
+ */
+public class mvcErrorController implements mvcErrorController {
+
+}
---
+++ src/main/java/net/liuxuan/spring/security/MyLoginUrlAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package net.liuxuan.spring.security;
+
+/**
+ * Copyright (c) 2010-2016.  by Liuxuan   All rights reserved.
+ * ***************************************************************************
+ * 源文件名:  net.liuxuan.spring.security.MyLoginUrlAuthenticationEntryPoint
+ * 功能:
+ * 版本:	@version 1.0
+ * 编制日期: 2016/2/19 10:31
+ * 修改历史: (主要历史变动原因及说明)
+ * YYYY-MM月DD |    Author      |	 Change Description
+ * 2016/2/19 |    Moses       |     Created
+ */
+public class MyLoginUrlAuthenticationEntryPoint {
+
+}
