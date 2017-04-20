8785cabbabeeef4e21db80b9ad7542635a3ecb19
mosliu <lx0319@gmail.com>
2017-04-20 15:31:16 +0800

    ehcahe3+jsr107工作不正常，改为ehcahe2。

---
+++ src/main/resources/static/ui/mosesadd/numfloating.js
@@ -0,0 +1,24 @@
+
+var _click_count=0;
+$("body").bind("click",function(e){
+    //var n=Math.round(Math.random()*100);//随机数
+    var $i=$("<b>").text("+"+(++_click_count));
+    var x=e.pageX,y=e.pageY;
+    $i.css({
+        "z-index":99999,
+        "top":y-15,
+        "left":x,
+        "position":"absolute",
+        "color":"red"
+    });
+    $("body").append($i);
+    $i.animate(
+        {"top":y-180,"opacity":0},
+        1500,
+        function(){$i.remove();}
+    );
+    e.stopPropagation();
+});
+/**
+ * Created by Moses on 2017/4/20.
+ */
